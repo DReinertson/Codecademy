f45a5272803cdcdbd5d6d966ba74c5cb398f42d1
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-08-22 01:10:49 -0700

    Update Parent.js

--- Child - Parent Components/Updating sibling/Parent.js
+++ Child - Parent Components/Updating sibling/Parent.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child';
+import { Sibling } from './Sibling';
 
-export class Child extends React.Component {
+class Parent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
   }
 
-  handleChange(e) {
-    const name = e.target.value;
-    this.props.onChange(name);
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
   }
 
   render() {
     return (
       <div>
-        <select
-          id="great-names"
-          onChange={this.handleChange}>
-
-          <option value="Frarthur">Frarthur</option>
-          <option value="Gromulus">Gromulus</option>
-          <option value="Thinkpiece">Thinkpiece</option>
-        </select>
+        <Child onChange={this.changeName} />
+        <Sibling name={this.state.name} />
       </div>
     );
   }
-}
+});
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
