6483b6bc9f98968fd51fca46e982f41a7959b8a3
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-09-02 19:12:07 -0700

    Create Sleep Debt Calculator.js

---
+++ Learn JavaScript/Functions/Sleep Debt Calculator.js
@@ -0,0 +1,49 @@
+const getSleepHours = day => {
+  switch(day){
+    case 'Monday':
+      return 8;
+      break;
+    case 'Tuesday':
+      return 7;
+      break;
+    case 'Wednesday':
+      return 6;
+      break;
+    case 'Thursday':
+      return 8;
+      break;
+    case 'Friday':
+      return 8;
+      break;
+    case 'Saturday':
+      return 10;
+      break;
+    case 'Sunday':
+      return 3;
+      break;
+  }
+}
+
+const getActualSleepHours = () => getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday');
+
+const getIdealSleepHours = () =>{
+  let idealHours = 7;
+  return idealHours *= 7;
+}
+
+const calculateSleepDebt = () =>{
+  let actualSleepHours = getActualSleepHours();
+  let idealSleepHours = getIdealSleepHours();
+  
+  if (actualSleepHours === idealSleepHours){
+    console.log("You got the perfect amount of sleep");
+  }
+  else if (actualSleepHours > idealSleepHours){
+    console.log(`You got more sleep than needed! By ${actualSleepHours - idealSleepHours} hours.`);
+  }
+  else{
+    console.log(`You should get some rest, you're behind in sleep by ${actualSleepHours - idealSleepHours}`);
+}
+}
+
+calculateSleepDebt();
