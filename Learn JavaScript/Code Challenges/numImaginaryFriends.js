81574b95a274ec290bf4b6e8d43b18b0a0ac4a9b
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-09-02 20:29:23 -0700

    Create numImaginaryFriends.js

---
+++ Learn JavaScript/Code Challenges/numImaginaryFriends.js
@@ -0,0 +1,11 @@
+// Write your function here:
+const numImaginaryFriends = totalFriends => Math.round(totalFriends * 0.33);
+
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(numImaginaryFriends(18)) // Should print 6
+
+// We encourage you to add more function calls of your own to test your code!
