d59017a10354067c5374929bcfa1ae433755ab43
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-09-02 20:22:48 -0700

    Create truthyOrFalsy.js

---
+++ Learn JavaScript/Code Challenges/truthyOrFalsy.js
@@ -0,0 +1,28 @@
+// Write your function here:
+
+
+const truthyOrFalsy = something => something ? true : false;
+
+/*function truthyOrFalsy(something){
+  if (something){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+*/
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(truthyOrFalsy(0)) // Should print false
+
+// We encourage you to add more function calls of your own to test your code!
+
+
+
+
+
+
