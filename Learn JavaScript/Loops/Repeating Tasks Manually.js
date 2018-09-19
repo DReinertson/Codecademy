435ce3c643d6772b7630be78bb79750f00683087
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-09-19 15:05:40 -0700

    Create Repeating Tasks Manually.js

---
+++ Learn JavaScript/Loops/Repeating Tasks Manually.js
@@ -0,0 +1,11 @@
+// Write your code below
+let vacationSpots = ['place1', 'place2', 'place3'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+
+
+
+
