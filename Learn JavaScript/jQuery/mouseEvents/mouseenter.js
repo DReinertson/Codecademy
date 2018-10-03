27c0b49d38e2be512fafea7c9dd3608e0303eba0
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-10-03 01:31:43 -0700

    Create mouseenter.js

---
+++ Learn JavaScript/jQuery/mouseEvents/mouseenter.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  $('.login-button').on('click', () => {
+    $('.login-form').show();
+  });
+  
+  $('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show();
+  })
+  
+
+}); 
+
