1502c2c36a2aa4fdc8df8de89084c56936db08f9
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-10-09 15:45:07 -0700

    Update Build a Library.js

--- Projects/JavaScript/Build a Library.js
+++ Projects/JavaScript/Build a Library.js
@@ -76,6 +76,7 @@ class CD extends Media{
     super(title);
     this._artist = artist;
     this._songs = songs;
+    this._shuffledSongs = [];
   }
   
   get artist(){
@@ -84,8 +85,10 @@ class CD extends Media{
   get songs(){
     return this._songs;
   }
-  shuffle(){
-    let input = this._songs;
+  // Fisher-Yates (aka Knuth) Shuffle
+  /*shuffle(){
+    this._shuffledSongs = this._songs;
+    let input = this._shuffledSongs;
     
     for (let x = input.length - 1; x >= 0; x--){
       let ranInd = Math.floor(Math.random() * (x +1));
@@ -94,7 +97,26 @@ class CD extends Media{
       input[ranInd] = input[x];
       input[x] = itemAtInd;
     }
-    return input
+    return this._shuffledSongs;
+  }*/
+  
+  shuffle(){
+    let arrayLength = this._songs.length;
+    let indexArr = [];
+    //Create array with values of indices availabe. Will remove number at index when shuffling song and attribute this to new object proprety in order to preserve the original order. 
+    for (let x = 0; x < arrayLength; x++){
+      indexArr.push(x);
+    }
+    //For loop to pull song at random from this._songs. Will remove index of random song from indexArr(to prevent repeats), continue until indexArr.length = 0;
+    for (let y = 0; y < arrayLength; y++){
+      let randomNumber = Math.floor(Math.random() * indexArr.length);
+     let randomIndex = indexArr[randomNumber];
+      console.log('indexArr:', indexArr, 'randomNumber:', randomNumber, 'randomIndex: ', randomIndex);
+      this._shuffledSongs.push(this._songs[randomIndex]);
+      indexArr.splice(randomNumber, 1)
+    }
+    
+    
   }
 }
 
@@ -115,3 +137,4 @@ console.log(speed.getAverageRating());
 
 const U2 = new CD('U2', 'U2', ['track1', 'track2','track3', 'track4', 'track5', 'track6', 'track7']);
 console.log(U2.shuffle());
+console.log(U2);
