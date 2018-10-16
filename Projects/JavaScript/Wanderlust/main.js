803ec413e0ad59cef8fcd9b038e94107074caec3
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-10-16 15:15:46 -0700

    Update main.js
    added APIKEY

--- Projects/JavaScript/Wanderlust/main.js
+++ Projects/JavaScript/Wanderlust/main.js
@@ -6,7 +6,7 @@ const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 
 // APIXU Info
-const apiKey = '';
+const apiKey = '4698a55916a8418b8a8195532181510';
 const forecastUrl = 'http://api.apixu.com/v1/current.xml?key=';
 
 // Page Elements
@@ -18,7 +18,7 @@ const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const formatDate = () =>{
+/*const formatDate = () =>{
   const todayDate = new Date();
   let month = todayDate.getMonth();
   if(month < 10){
@@ -32,7 +32,30 @@ const formatDate = () =>{
   const fullDate = `${year}${month}${day}`
   return fullDate;
 }
+*/
+
+const today = new Date();
+today.month = today.getMonth();
+today.year = today.getFullYear();
+today.day = today.getDate();
+console.log(today.day);
+
+const formatDate = () =>{
+  let month = today.month;
+  if(month < 10){
+    month = "0" + month;
+  };
+  const year = today.year;
+  let day = today.day;
+  if(day < 10){
+    day = "0" + day;
+  };
+  const fullDate = `${year}${month}${day}`;
+  return fullDate;
+}
+
 const date = formatDate();
+
 
 // Add AJAX functions here:
 const getVenues = async() => {
@@ -57,6 +80,7 @@ const getVenues = async() => {
 const getForecast = async () => {
   const urlToFetch = `${forecastUrl}${apiKey}&q=${$input.val()}&days=4&hour=11`
   try{
+    console.log(urlToFetch);
     const response = await fetch(urlToFetch)
     if(response.ok){
       const jsonResponse = await response.json();
@@ -87,7 +111,10 @@ const renderVenues = (venues) => {
 const renderForecast = (days) => {
   $weatherDivs.forEach(($day, index) => {
     // Add your code here:
-    
+    //console.log(today.getDay());
+    //const currentDay = weekDays[today.getDay()]
+    const currentDay = days[index];
+    console.log('currentDay', currentDay);
 
 
     let weatherContent = createWeatherHTML(currentDay);
