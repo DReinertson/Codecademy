287f2202877adfa949ccad67ea4eaba8ab95417e
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-10-28 10:36:48 -0700

    Create Finding Documentation.js

---
+++ Debugging/Finding Documentation.js
@@ -0,0 +1,9 @@
+// Link to String.repeat() documentation: 
+function doubleString(string) {
+  return string.repeat(2);
+}
+
+// Should return 'echoecho'
+console.log("doubleString('echo') returns: " + doubleString('echo'));
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
